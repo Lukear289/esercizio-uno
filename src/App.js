ecbe470c8ad07eae37f4bce3545db058fd2aa50a
Luca Armagno <luca.armagno289@gmail.com>
2024-01-29 16:51:10 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import ButtonComponent from './ButtonComponent';
+import ImageComponent from './ImageComponent';
 function App() {
   return (
     <div className="App">
@@ -8,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <ButtonComponent nome="clicca" />
         <p>Primo esercizio react</p>
-
+        <ImageComponent srcphoto="https://placedog.net/200" />
         <ButtonComponent nome="cliccami" />
       </header>
     </div>
--- src/ImageComponent.jsx
+++ src/ImageComponent.jsx
@@ -1,2 +1,7 @@
 import { Component } from 'react';
-class ImageComponent extends Component {}
+class ImageComponent extends Component {
+  render() {
+    return <img src={this.props.srcphoto} alt="immagine" />;
+  }
+}
+export default ImageComponent;
